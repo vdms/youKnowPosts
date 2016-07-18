2409d7af29bcf8179b49e0ff3d169fa4a3d1ff2a
Vicente Sarmento <sarmento.vicente@gmail.com>
2016-07-18 14:10:56 -0300

    Tentando ajustar publicação dos posts.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var gulp        = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	markdown    = require('gulp-markdown'),
 	marked      = require('marked'),
-	inject      = require('gulp-inject'),
+	gfi         = require("gulp-file-insert"),
     debug       = require('gulp-debug'),
     tap         = require('gulp-tap'),
     path        = require('path'),
     rename      = require('gulp-rename'),
+    clean       = require('gulp-clean'),
+    run         = require('run-sequence'),
     foreach     = require('gulp-foreach');
 
 // Compile sass into CSS & auto-inject into browsers
@@ -18,6 +20,12 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('clean', function () {
+	return gulp.src([
+            './dist/*.html',
+            './src/md/*.html'], {read: false})
+            .pipe(clean());
+});
 
 // Markdown related
 marked.setOptions({
@@ -29,14 +37,14 @@ marked.setOptions({
 // Compila markdown
 gulp.task('markdown', function() {
     return gulp.src('./src/md/*.md')
-        .pipe(markdown())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }));
+                .pipe(markdown())
+                .pipe(gulp.dest(function(f) {
+                    return f.base;
+                }));
 });
 
 // Make a copy for each compiled md content
-gulp.task('md-insert', function() {
+gulp.task('create', function() {
   return gulp.src('./src/md/*.html')
             .pipe(foreach(function(stream, file) {
                 return stream
@@ -47,27 +55,33 @@ gulp.task('md-insert', function() {
                             .pipe(gulp.dest('./dist', {
                                 overwrite: true
                             }));
-
-                        gulp.src('./dist/' + fileName)
-                        .pipe(debug())
-                            .pipe(inject(
-                                gulp.src('./src/md/' + fileName),  {
-                                    starttag: '<!-- inject:head:{{ext}} -->',
-                                    transform: function (filePath, file) {
-                                        // return file contents as string
-                                        return file.contents.toString('utf8')
-                                    }
-                                }
-                            ))
-                            .pipe(gulp.dest('./dist/'));
                     }))
+        }))
+});
 
+gulp.task('inject', function() {
+    return gulp.src('./dist/*.*')
+                .pipe(debug())
+                .pipe(foreach(function(stream, file) {
+                    return stream
+                        .pipe(tap(function (file, t) {
+                                var fileName = path.basename(file.path).toString();
+                                var filePath = file.path;
+                                gulp.src(filePath)
+                                    .pipe(gfi({"<!-- post content -->" : "./src/md/" + fileName}))
+                                    .pipe(gulp.dest('./dist/'))
+                        }))
+                }))
+});
 
-            }))
+gulp.task('build', function(callback) {
+  run('clean', 'markdown', 'create', ['inject'], function() {
+      callback();
   });
+});
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'markdown', 'md-insert'], function() {
+gulp.task('serve', ['sass', 'build'], function() {
 
     browserSync.init({
         server: "./dist",
@@ -75,23 +89,9 @@ gulp.task('serve', ['sass', 'markdown', 'md-insert'], function() {
     });
 
     gulp.watch("./src/scss/*.scss", ['sass']);
-    gulp.watch(['./_base.html'], ['md-insert']);
-    gulp.watch(['./src/md/*.md'], ['md-insert']);
+    gulp.watch(['./_base.html'], ['build']);
+    gulp.watch(['./src/md/'], ['build']);
     gulp.watch("./src/*.html").on('change', browserSync.reload);
 });
 
-
 gulp.task('default', ['serve']);
-
-// gulp.task('md-insert', function() {
-//   return  gulp.src('./_base.html')
-//             .pipe(inject(
-//                 gulp.src(['./src/md/*.html']), {
-//                     starttag: '<!-- inject:head:{{ext}} -->',
-//                     transform: function (filePath, file) {
-//                         // return file contents as string
-//                         return file.contents.toString('utf8')
-//                     }
-//             }))
-//             .pipe(gulp.dest('./dist/' + fileName + '.html'));
-// });
